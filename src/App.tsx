904ddf28a0da37ff7a7a92482d37f851aa108fa7
MoonPillGyeom <148839794+MoonPillGyeom@users.noreply.github.com>
2024-03-04 12:03:43 +0900

    라우팅 정리 (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import SchedulesPage from "./pages/SchedulesPage";
 import TeamsPage from "./pages/TeamsPage";
 import TeamsPostsPage from "./pages/TeamsPostsPage";
 import MyIssuesPage from "./pages/MyIssuesPage";
-// import MyIssuesPage from "@/pages/MyIssuesPage";
 
 function App() {
   return (
@@ -19,8 +18,10 @@ function App() {
         <Route path="/main" element={<MainPage />} />
         <Route path="/schedules/:userId" element={<SchedulesPage />} />
         <Route path="/myIssues/:userId" element={<MyIssuesPage />} />
-        <Route path="/teams/:teamsId" element={<TeamsPage />} />
-        <Route path="/teams/:teamsId/posts" element={<TeamsPostsPage />} />
+        <Route path="/teams">
+          <Route path=":teamsId" element={<TeamsPage />} />
+          <Route path=":teamsId/posts" element={<TeamsPostsPage />} />
+        </Route>
       </Routes>
     </Router>
   );
