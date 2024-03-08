bee25b229744a6da07a77d8d66a96d1ea583835b
Jisun Park <jisunpark.t@gmail.com>
2024-03-09 00:15:50 +0900

    Design: Create group section in LNB

--- src/components/common/SideBar/SideBar.tsx
+++ src/components/common/SideBar/SideBar.tsx
@@ -1,33 +1,39 @@
 import { useState } from 'react';
-import GroupEditModal from '../../Modal/GroupEditModal';
+import CreateIcon from '../../../../public/assets/plus-circle-light.svg';
 import GroupModal from '../../Modal/GroupModal';
-import BoardItem from './BoardItem';
 import BoardItems from './BoardItem';
+import clsx from 'clsx';
 
 export default function SideBar() {
   const [isOpen, setIsOpen] = useState(false);
-  const [isEditOpen, setIsEditOpen] = useState(false);
 
-  const handleButtonClick = () => {
+  const handleCreateClick = () => {
     setIsOpen(!isOpen);
-  };
-
-  const handleEditButtonClick = () => {
-    setIsEditOpen(!isOpen);
   };
 
   const handleCloseClick = () => {
     setIsOpen(false);
   };
 
-  const handleCloseEditClick = () => {
-    setIsEditOpen(false);
-  };
-
   return (
-    <div className="fixed bottom-0 left-0 top-[8.2rem] w-[26rem] rounded-tr-3xl bg-[#292929]">
-      <BoardItems />
-    </div>
+    <>
+      {isOpen && <GroupModal closeClick={handleCloseClick} />}
+      <div
+        className={clsx(`fixed bottom-0 left-0 top-[8.2rem] w-[26rem] rounded-tr-3xl bg-[#292929]`)}
+      >
+        <BoardItems />
+        <div
+          className={clsx(
+            `mt-[47.8rem] flex items-center justify-between bg-[#222222] py-[1.8rem] pl-16 pr-[2.4rem]`,
+          )}
+        >
+          <span className={clsx(`text-[1.6rem] font-bold text-[#EDEEDC]`)}>그룹</span>
+          <button onClick={handleCreateClick}>
+            <img src={CreateIcon} alt="그룹 생성 버튼" />
+          </button>
+        </div>
+      </div>
+    </>
   );
 
   // return (
