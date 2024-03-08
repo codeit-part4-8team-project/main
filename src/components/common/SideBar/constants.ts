9c0d6acaa396b23bb8c9774fdb4b73b7ef00270f
Jisun Park <jisunpark.t@gmail.com>
2024-03-08 23:51:18 +0900

    Feat: Implement BoardItems of SideBar

---
+++ src/components/common/SideBar/constants.ts
@@ -0,0 +1,37 @@
+import BoxOffIcon from '../../../../public/assets/box-off.svg';
+import BoxOnIcon from '../../../../public/assets/box-on.svg';
+import CalendarOffIcon from '../../../../public/assets/calendar-off.svg';
+import CalendarOnIcon from '../../../../public/assets/calendar-on.svg';
+import SectionOffIcon from '../../../../public/assets/section-off.svg';
+import SectionOnIcon from '../../../../public/assets/section-on.svg';
+
+interface Board {
+  [key: string]: string;
+}
+
+export interface Boards {
+  dashboard: Board;
+  calendar: Board;
+  kanbanboard: Board;
+}
+
+export const BOARDS: Boards = {
+  dashboard: {
+    boardName: '대시보드',
+    iconOn: BoxOnIcon,
+    iconOff: BoxOffIcon,
+    link: '/teams/1',
+  },
+  calendar: {
+    boardName: '나의 캘린더',
+    iconOn: CalendarOnIcon,
+    iconOff: CalendarOffIcon,
+    link: '/schedules/1',
+  },
+  kanbanboard: {
+    boardName: '칸반보드',
+    iconOn: SectionOnIcon,
+    iconOff: SectionOffIcon,
+    link: '/myIssues/1',
+  },
+};
