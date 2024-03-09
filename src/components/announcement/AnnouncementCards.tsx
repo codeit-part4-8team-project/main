ff5d84dd75d61f767b10a0df3f9fa89574e20d5a
Jisun Park <148641571+jisunipark@users.noreply.github.com>
2024-03-09 14:33:10 +0900

    Design: 공지사항 컴포넌트 UI 구현 (#26)
    * Design: construct UI of AnnouncementBoard
    
    * Design: Change existing icons into new icons
    
    * Chore: Add clsx in import order setting
    
    * Design: Remove unnecessary clsx function

--- src/components/announcement/AnnouncementCards.tsx
+++ src/components/announcement/AnnouncementCards.tsx
@@ -1,52 +1,51 @@
-import checkIcon from '../../../public/check.svg';
-import circleIcon from '../../../public/circle.svg';
-import closedIcon from '../../../public/closed.svg';
-import profileImg from '../../../public/profile.svg';
+import { useState } from 'react';
+import ArrowDownIcon from '../../../public/assets/arrow-down-dark.png';
+import CheckIcon from '../../../public/assets/check-circle-dark.svg';
+import CloseIcon from '../../../public/assets/hidden-dark.svg';
+import ProfileImg from '../../../public/assets/profile-small.svg';
 
 function AnnouncementCard() {
+  const content =
+    '떠나는 길에 니가 내게 말했지. 너는 바라는 게 너무나 많아. 잠깐이라도 널 안 바라보면 머리에 불이 나버린다니까 나는 흐르려는 눈물을 참고 하려던 얘길 어렵게 누르고 그래 미안해 라는 한 마디로 너랑 나눈 날들 마무리했었지 달디달고 달디달고 달디단 밤양갱 밤양갱 내가 먹고 싶었던 건 달디단 밤양갱 밤양갱이야 떠나는 길에 니가 내게 말했지 너는 바라는 게 너무나 많아 아냐 내가 늘 바란 건 하나야';
+  const shortened = content.substring(0, 75) + '...';
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggleContent = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="flex h-fit w-[41.2rem] flex-col gap-[1.8rem] bg-[#D9D9D9] p-[2.2rem]">
-      <div className="flex justify-between">
-        <div className="flex gap-[0.8rem]">
-          <img src={circleIcon} alt="동그라미 아이콘" />
-          <span className="text-[1.4rem] font-bold">팀 1</span>
-        </div>
-        <div className="flex gap-[0.8rem]">
-          <img src={checkIcon} alt="체크 표시 아이콘" />
-          {/* TODO 펼치기 & 닫기 기능 */}
-          <img src={closedIcon} alt="닫힘 표시 아이콘" />
-        </div>
+    <div className="relative flex w-[42.6rem] flex-col gap-[1.7rem] rounded-[2.4rem] bg-[#ECF619] p-[2.4rem]">
+      <span className="text-[1.4rem] font-bold text-[#292929]">디자인 스터디</span>
+      <span className="text-[1.4rem] text-[#292929]">{isOpen ? content : shortened}</span>
+      <span className="text-40 text-[#222222] opacity-30">게시 : 2024-03-04</span>
+      <div className="absolute right-[2.4rem] top-[2.4rem] flex items-center gap-[0.8rem]">
+        <button type="button">
+          <img className="h-[2.4rem] w-[2.4rem]" src={CheckIcon} alt="체크 버튼" />
+        </button>
+        <button type="button" onClick={handleToggleContent}>
+          {isOpen ? (
+            <img className="m-[0.4rem] w-[1.6rem] opacity-65" src={CloseIcon} alt="닫힘 아이콘" />
+          ) : (
+            <img className="h-[2.4rem] w-[2.4rem]" src={ArrowDownIcon} alt="펼치기 아이콘" />
+          )}
+        </button>
       </div>
-      <span>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore vero rem omnis recusandae
-        pariatur laborum, aliquid quaerat deserunt at consectetur sequi totam reprehenderit
-        temporibus libero id minima! Quasi, debitis quos? Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Modi officiis distinctio aperiam. Fugiat inventore, illum quisquam
-        corporis libero earum voluptatum tenetur laboriosam commodi error. Error, sequi. Commodi
-        unde enim ad. Optio ipsum voluptates distinctio inventore vitae fugit sed sapiente ullam?
-        Accusamus quidem necessitatibus quo, suscipit non esse atque fugit distinctio earum, aliquam
-        ipsa illo? Nihil laudantium distinctio quo obcaecati animi! Corrupti, nostrum voluptatibus
-        mollitia nobis assumenda officiis sunt laborum culpa pariatur veniam ad quod explicabo
-        architecto ex eos corporis. Impedit maiores corrupti soluta cumque iure, iste ratione amet
-        recusandae repudiandae.
-      </span>
-      <div className="flex justify-between text-[1rem] text-[#969696]">
-        <span>2024-02-03</span>
-        <div className="flex gap-[0.8rem]">
-          <span>태그된 사람</span>
-          <div className="flex">
-            <img className="mr-[-10px]" src={profileImg} alt="프로필 이미지" />
-            <img className="mr-[-10px]" src={profileImg} alt="프로필 이미지" />
-          </div>
-        </div>
-      </div>
+      <button type="button">
+        <img
+          className="absolute bottom-[2.4rem] right-[2.4rem] h-[2.4rem] w-[2.4rem]"
+          src={ProfileImg}
+          alt="프로필 이미지"
+        />
+      </button>
     </div>
   );
 }
 
 export default function AnnouncementCards() {
   return (
-    <div className="flex flex-col gap-[1.5rem]">
+    <div className="flex flex-col gap-[2.4rem]">
       <AnnouncementCard />
       <AnnouncementCard />
       <AnnouncementCard />
