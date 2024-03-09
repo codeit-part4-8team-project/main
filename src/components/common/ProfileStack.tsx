7202d301da95123c15cb1f1e2d86cadeb67dd413
Jisun Park <jisunpark.t@gmail.com>
2024-03-09 14:50:10 +0900

    Design: Construct UI of IssueList

--- src/components/common/ProfileStack.tsx
+++ src/components/common/ProfileStack.tsx
@@ -2,31 +2,15 @@ interface ProfileStackProps {
   profileImgs: string[];
 }
 
-const STYLE = {
-  0: {
-    zIndex: 20,
-    marginRight: -10,
-  },
-  1: {
-    zIndex: 10,
-    marginRight: -10,
-  },
-  2: {
-    zIndex: 0,
-    marginRight: 0,
-  },
-};
-
-// 프로필 이미지 경로 배열에 담아서 프롭으로 내려줌
+// 팀원의 프로필 이미지의 경로가 담긴 배열을 프롭으로 받아 최대 3명의 프로필을 보여주는 컴포넌트
 export default function ProfileStack({ profileImgs }: ProfileStackProps) {
-  // const profileCount = profileImgs.length;
+  profileImgs = profileImgs.slice(0, 3);
+
   return (
     <div className="flex ">
-      {/* <img className="z-20 mr-[-10px] h-[2.4rem] w-[2.4rem]" src={profileImgs[profileCount - 3]} />
-      <img className="z-10 mr-[-10px] h-[2.4rem] w-[2.4rem]" src={profileImgs[profileCount - 2]} />
-      <img className="z-0 h-[2.4rem] w-[2.4rem]" src={profileImgs[profileCount - 1]} /> */}
       {profileImgs.map((profile, idx) => {
-        const { zIndex, marginRight } = STYLE[idx as keyof typeof STYLE];
+        const zIndex = idx === 0 ? 20 : idx === 1 ? 10 : 0;
+        const marginRight = idx === profileImgs.length - 1 ? 0 : -10;
         return (
           <img
             className={`mr-[${marginRight}px] z-${zIndex} h-[2.4rem] w-[2.4rem]`}
