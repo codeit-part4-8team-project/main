97789ea9a8549820c2e7c95a66934fcfd5aee9e0
Jisun Park <jisunpark.t@gmail.com>
2024-03-09 12:05:18 +0900

    Design: Change existing icons into new icons

--- src/components/announcement/AnnouncementBoard.tsx
+++ src/components/announcement/AnnouncementBoard.tsx
@@ -1,4 +1,4 @@
-import checkIcon from '../../../public/check.svg';
+import MegaphoneIcon from '../../../public/assets/megaphone-dark.svg';
 import AnnouncementCards from './AnnouncementCards';
 
 export default function AnnouncementBoard() {
@@ -15,7 +15,7 @@ export default function AnnouncementBoard() {
 function AnnouncememntNotification() {
   return (
     <div className="flex gap-4">
-      <img src={checkIcon} alt="메가폰 아이콘" />
+      <img src={MegaphoneIcon} alt="메가폰 아이콘" />
       <span className="text-[1.6rem] font-medium">
         새로운 공지사항이 총 <span className="font-extrabold">3</span>건이 있습니다.
       </span>
--- src/components/announcement/AnnouncementCards.tsx
+++ src/components/announcement/AnnouncementCards.tsx
@@ -1,11 +1,8 @@
 import { useState } from 'react';
-import checkIcon from '../../../public/check.svg';
-// 교체예정
-import closedIcon from '../../../public/closed.svg';
-// 교체예정
-import dropdownIcon from '../../../public/images/dorpdown.svg';
-// 교체예정
-import profileImg from '../../../public/profile.svg';
+import ArrowDownIcon from '../../../public/assets/arrow-down-dark.png';
+import CheckIcon from '../../../public/assets/check-circle-dark.svg';
+import CloseIcon from '../../../public/assets/hidden-dark.svg';
+import ProfileImg from '../../../public/assets/profile-small.svg';
 import clsx from 'clsx';
 
 function AnnouncementCard() {
@@ -28,27 +25,30 @@ function AnnouncementCard() {
       <span className={clsx('text-[1.4rem] font-bold text-[#292929]')}>디자인 스터디</span>
       <span className={clsx('text-[1.4rem] text-[#292929]')}>{isOpen ? content : shortened}</span>
       <span className={clsx('text-40 text-[#222222] opacity-30')}>게시 : 2024-03-04</span>
-      {/* TODO 아이콘 교체 */}
       <div className={clsx('absolute right-[2.4rem] top-[2.4rem] flex items-center gap-[0.8rem]')}>
         <button type="button">
-          <img
-            className={clsx('m-[0.5rem] h-[1.4rem] w-[1.4rem]')}
-            src={checkIcon}
-            alt="체크 버튼"
-          />
+          <img className={clsx('h-[2.4rem] w-[2.4rem]')} src={CheckIcon} alt="체크 버튼" />
         </button>
         <button type="button" onClick={handleToggleContent}>
           {isOpen ? (
-            <img className={clsx('h-[1.6rem] w-[1.6rem]')} src={closedIcon} alt="닫힘 아이콘" />
+            <img
+              className={clsx('m-[0.4rem] w-[1.6rem] opacity-65')}
+              src={CloseIcon}
+              alt="닫힘 아이콘"
+            />
           ) : (
-            <img className={clsx('h-[1.6rem] w-[1.6rem]')} src={dropdownIcon} alt="펼치기 아이콘" />
+            <img
+              className={clsx('h-[2.4rem] w-[2.4rem]')}
+              src={ArrowDownIcon}
+              alt="펼치기 아이콘"
+            />
           )}
         </button>
       </div>
       <button type="button">
         <img
           className={clsx('absolute bottom-[2.4rem] right-[2.4rem] h-[2.4rem] w-[2.4rem]')}
-          src={profileImg}
+          src={ProfileImg}
           alt="프로필 이미지"
         />
       </button>
