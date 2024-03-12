31f38580cb04b0cfbe5981092803146af70068ee
MoonPillGyeom <ansvkf0820@naver.com>
2024-03-12 16:15:29 +0900

    Feat: Modal change

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
+import ModalLayout from '@/components/common/ModalLayout';
 import Nav from '@/components/common/Nav';
 
 const HomePage = () => {
   return (
     <>
       <Nav />
-      <div>홈페이지입니다.</div>
+      {/* <div>홈페이지입니다.</div> */}
+      <ModalLayout />
     </>
   );
 };
