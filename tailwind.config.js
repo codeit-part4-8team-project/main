dc147d069b6f7ae62e23b86cf5aa8f157f7f8d8e
case <121015462+caseV12@users.noreply.github.com>
2024-03-14 04:44:35 +0900

    Setting: Add color variable on tailwindconfig (#40)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      white: '#FFFFFF',
+      gray10: '#F7F7F7',
+      gray20: '#EFEFEF',
+      gray30: '#E5E5E5',
+      gray50: '#A1A1A1',
+      gray80: '5F5F5F',
+      gray100: '#292929',
+      black: '#222222',
+      point_yellow: '#F3FF00',
+      point_red: '#F74242',
+    },
     extend: {},
     fontFamily: {
       rammetto: ['"Rammetto One"'],
