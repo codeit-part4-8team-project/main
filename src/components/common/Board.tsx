0928a45c5e591526a55d1df93cf696f42e8492aa
Jisun Park <jisunpark.t@gmail.com>
2024-03-15 00:34:31 +0900

    Refactor: Define BoardProp type

--- src/components/common/Board.tsx
+++ src/components/common/Board.tsx
@@ -1,6 +1,10 @@
 import { ReactNode } from 'react';
 
-function Board({ children }: { children: ReactNode }) {
+interface BoardProp {
+  children: ReactNode;
+}
+
+function Board({ children }: BoardProp) {
   return (
     <div className="h-full w-full rounded-[2.4rem] bg-gray10 p-12 shadow-[0px_0px_5px_0px_rgba(17,17,17,0.1)]">
       {children}
