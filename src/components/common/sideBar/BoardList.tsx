cb30086b1cc607b0a2ede1e5238390057425ac2f
Jisun Park <148641571+jisunipark@users.noreply.github.com>
2024-03-15 19:59:37 +0900

    Add files via upload

---
+++ src/components/common/sideBar/BoardList.tsx
@@ -0,0 +1,52 @@
+import { Link, useLocation } from 'react-router-dom';
+import clsx from 'clsx';
+import { BOARDS, Boards } from '@/components/common/sideBar/constants';
+
+interface BoardItemProps {
+  boardType: string;
+  pathname: string;
+}
+
+export default function BoardList() {
+  const { pathname } = useLocation();
+
+  return (
+    <ul className="absolute left-[2.4rem] top-40 flex flex-col gap-[1.6rem]">
+      <li>
+        <BoardItem boardType="dashboard" pathname={pathname} />
+      </li>
+      <li>
+        <BoardItem boardType="calendar" pathname={pathname} />
+      </li>
+      <li>
+        <BoardItem boardType="kanbanboard" pathname={pathname} />
+      </li>
+      <li>
+        <BoardItem boardType="board" pathname={pathname} />
+      </li>
+    </ul>
+  );
+}
+
+function BoardItem({ boardType, pathname }: BoardItemProps) {
+  const { boardName, iconOn, iconOff, link } = BOARDS[boardType as keyof Boards];
+  const isCurrent = pathname === link ? true : false;
+
+  return (
+    <Link to={link}>
+      <button
+        className={clsx(
+          'flex h-16 w-[21.2rem] items-center gap-[1.6rem] rounded-[0.6rem] px-[1.6rem] py-[0.8rem] hover:bg-[#EDEEDC]/10',
+          isCurrent && 'bg-[#EDEEDC]/10',
+        )}
+      >
+        <img
+          className={clsx(!isCurrent && 'opacity-10')}
+          src={isCurrent ? iconOn : iconOff}
+          alt={`${boardName} 아이콘`}
+        />
+        <span className="text-body3-regular text-[#EDEEDC] opacity-100">{boardName}</span>
+      </button>
+    </Link>
+  );
+}
---
+++ src/components/common/sideBar/constants.ts
@@ -0,0 +1,46 @@
+import BoxOffIcon from '../../../../public/assets/box-off.svg';
+import BoxOnIcon from '../../../../public/assets/box-on.svg';
+import CalendarOffIcon from '../../../../public/assets/calendar-off.svg';
+import CalendarOnIcon from '../../../../public/assets/calendar-on.svg';
+import FolderOffIcon from '../../../../public/assets/folder-off.svg';
+import FolderOnIcon from '../../../../public/assets/folder-on.svg';
+import SectionOffIcon from '../../../../public/assets/section-off.svg';
+import SectionOnIcon from '../../../../public/assets/section-on.svg';
+
+interface Board {
+  [key: string]: string;
+}
+
+export interface Boards {
+  dashboard: Board;
+  calendar: Board;
+  kanbanboard: Board;
+  board: Board;
+}
+
+export const BOARDS: Boards = {
+  dashboard: {
+    boardName: '대시보드',
+    iconOn: BoxOnIcon,
+    iconOff: BoxOffIcon,
+    link: '/main',
+  },
+  calendar: {
+    boardName: '나의 캘린더',
+    iconOn: CalendarOnIcon,
+    iconOff: CalendarOffIcon,
+    link: '/schedules/1',
+  },
+  kanbanboard: {
+    boardName: '칸반보드',
+    iconOn: SectionOnIcon,
+    iconOff: SectionOffIcon,
+    link: '/myIssues/1',
+  },
+  board: {
+    boardName: '자유게시판',
+    iconOn: FolderOnIcon,
+    iconOff: FolderOffIcon,
+    link: '/teams/1/posts',
+  },
+};
