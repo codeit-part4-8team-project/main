d7759fe42f3f58a842182e11b7ecb974247ec41c
Jisun Park <148641571+jisunipark@users.noreply.github.com>
2024-03-15 10:12:17 +0900

    Design: 메인 페이지 ui 높이 수정 및 색상 변수 적용 (#43)
    * Refactor: Use content prop instead of children prop in BoardSection
    
    * Refactor: Remove unnecessary div tag in MainPage
    
    * Design: Fix element height based on 1080 screen height
    
    * Design: Fix color names into variable names

--- src/components/kanbanBoard/KanbanBoard.tsx
+++ src/components/kanbanBoard/KanbanBoard.tsx
@@ -6,8 +6,8 @@ interface KanbanBoardItemProps {
 
 function KanbanBoardItem({ title }: KanbanBoardItemProps) {
   return (
-    <div className="flex h-[52.3rem] w-[34.2rem] flex-col gap-[2.4rem] rounded-[2.4rem] bg-[#FCFCFC] px-12 pt-12 shadow-[0_0_1rem_0_rgba(17,17,17,0.05)]">
-      <span className="text-[1.6rem] font-bold text-[#5F5F5F]">{title}</span>
+    <div className="flex h-[47.9rem] w-full flex-col gap-[2.4rem] rounded-[2.4rem] bg-[#FCFCFC] px-12 pt-12 shadow-[0_0_1rem_0_rgba(17,17,17,0.05)]">
+      <span className="text-[1.6rem] font-bold text-gray80">{title}</span>
       <IssueList />
     </div>
   );
@@ -15,7 +15,7 @@ function KanbanBoardItem({ title }: KanbanBoardItemProps) {
 
 export default function KanbanBoard() {
   return (
-    <div className="flex w-full justify-between gap-[2.4rem]">
+    <div className="flex h-full w-full justify-between gap-[2.4rem]">
       <KanbanBoardItem title="할 일 3" />
       <KanbanBoardItem title="진행 중 1" />
       <KanbanBoardItem title="백로그 1" />
