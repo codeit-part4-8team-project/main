496aaee2852982629f37e69ddd0e86d140f029a7
case <121015462+caseV12@users.noreply.github.com>
2024-03-15 18:25:37 +0900

    Setting: tailwind Typography 설정 (#48)
    * Setting: configure custom class of fonts
    
    * Setting: modify typography class name

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -13,6 +14,23 @@ export default {
       black: '#222222',
       point_yellow: '#F3FF00',
       point_red: '#F74242',
+    },
+    fontSize: {
+      'body1-bold': ['1.8rem', { lineHeight: '2.6rem', fontWeight: 700 }],
+      'body1-medium': ['1.8rem', { lineHeight: '2.6rem', fontWeight: 500 }],
+      'body1-regular': ['1.8rem', { lineHeight: '2.6rem', fontWeight: 400 }],
+      'body2-bold': ['1.6rem', { lineHeight: '2.4rem', fontWeight: 700 }],
+      'body2-medium': ['1.6rem', { lineHeight: '2.4rem', fontWeight: 500 }],
+      'body2-regular': ['1.6rem', { lineHeight: '2.4rem', fontWeight: 400 }],
+      'body3-bold': ['1.4rem', { lineHeight: '2.2rem', fontWeight: 700 }],
+      'body3-medium': ['1.4rem', { lineHeight: '2.2rem', fontWeight: 500 }],
+      'body3-regular': ['1.4rem', { lineHeight: '2.2rem', fontWeight: 400 }],
+      'body4-bold': ['1.2rem', { lineHeight: '2.0rem', fontWeight: 700 }],
+      'body4-medium': ['1.2rem', { lineHeight: '2.0rem', fontWeight: 500 }],
+      'body4-regular': ['1.2rem', { lineHeight: '2.0rem', fontWeight: 400 }],
+      'body5-bold': ['1.0rem', { lineHeight: '1.8rem', fontWeight: 700 }],
+      'body5-medium': ['1.0rem', { lineHeight: '1.8rem', fontWeight: 500 }],
+      'body5-regular': ['1.0rem', { lineHeight: '1.8rem', fontWeight: 400 }],
     },
     extend: {},
     fontFamily: {
