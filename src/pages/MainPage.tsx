c686cfc2b57443e9869464fb08457e6826c119e3
Jisun Park <148641571+jisunipark@users.noreply.github.com>
2024-03-15 10:13:19 +0900

    Refactor: 기본 레이아웃을 공통 컴포넌트로 분리 (#44)
    * Refactor: Use content prop instead of children prop in BoardSection
    
    * Refactor: Remove unnecessary div tag in MainPage
    
    * Design: Fix element height based on 1080 screen height
    
    * Design: Fix color names into variable names
    
    * Refactor: Construct common page layout
    
    * Refactor: Define BoardProp type

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,20 +1,17 @@
 import BoardSection from '@/components/common/BoardSection';
-import Nav from '@/components/common/Nav';
-import SideBar from '@/components/common/sideBar/SideBar';
+import PageLayout from '@/components/common/PageLayout';
 import AnnouncementBoard from '@/components/announcement/AnnouncementBoard';
 import KanbanBoard from '@/components/kanbanBoard/KanbanBoard';
 
 export default function MainPage() {
   return (
-    <div className="h-screen w-screen bg-gray20 pb-[4.4rem] pl-[28.4rem] pr-[2.4rem] pt-[8.2rem]">
-      <Nav />
-      <SideBar />
-      <div className="grid h-full w-full grid-cols-[107.4fr_37.8fr] grid-rows-[33.7fr_52.5fr] gap-[5.2rem] rounded-[2.4rem] bg-gray10 p-12 shadow-[0px_0px_5px_0px_rgba(17,17,17,0.1)]">
+    <PageLayout>
+      <div className="grid h-full w-full grid-cols-[107.4fr_37.8fr] grid-rows-[33.7fr_52.5fr] gap-[5.2rem]">
         <BoardSection title="My calendar" content={<Temp />} />
         <AnnouncementBoard />
         <BoardSection title="Kanban board" content={<KanbanBoard />} />
       </div>
-    </div>
+    </PageLayout>
   );
 }
 
