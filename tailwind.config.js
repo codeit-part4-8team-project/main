2c6f0c4edac757c6a595131b5f9b5798574a6611
caseV12 <caseVDev@gmail.com>
2024-03-15 16:06:51 +0900

    Setting: configure custom class of fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -19,5 +20,86 @@ export default {
       rammetto: ['"Rammetto One"'],
     },
   },
-  plugins: [],
+  plugins: [
+    /** @type {import('tailwindcss/types/config').PluginCreator} */
+    ({ addUtilities }) => {
+      addUtilities({
+        '.body1-bold': {
+          'font-size': '1.8rem',
+          'font-weight': 700,
+          'line-height': '2.6rem',
+        },
+        '.body1-medium': {
+          'font-size': '1.8rem',
+          'font-weight': 500,
+          'line-height': '2.6rem',
+        },
+        '.body1-regular': {
+          'font-size': '1.8rem',
+          'font-weight': 400,
+          'line-height': '2.6rem',
+        },
+        '.body2-bold': {
+          'font-size': '1.6rem',
+          'font-weight': 700,
+          'line-height': '2.4rem',
+        },
+        '.body2-medium': {
+          'font-size': '1.6rem',
+          'font-weight': 500,
+          'line-height': '2.4rem',
+        },
+        '.body2-regular': {
+          'font-size': '1.6rem',
+          'font-weight': 400,
+          'line-height': '2.4rem',
+        },
+        '.body3-bold': {
+          'font-size': '1.4rem',
+          'font-weight': 700,
+          'line-height': '2.2rem',
+        },
+        '.body3-medium': {
+          'font-size': '1.4rem',
+          'font-weight': 500,
+          'line-height': '2.2rem',
+        },
+        '.body3-regular': {
+          'font-size': '1.4rem',
+          'font-weight': 400,
+          'line-height': '2.2rem',
+        },
+        '.body4-bold': {
+          'font-size': '1.2rem',
+          'font-weight': 700,
+          'line-height': '2.0rem',
+        },
+        '.body4-medium': {
+          'font-size': '1.2rem',
+          'font-weight': 500,
+          'line-height': '2.0rem',
+        },
+        '.body4-regular': {
+          'font-size': '1.2rem',
+          'font-weight': 400,
+          'line-height': '2.0rem',
+        },
+        '.body5-bold': {
+          'font-size': '1.0rem',
+          'font-weight': 700,
+          'line-height': '1.8rem',
+        },
+        '.body5-medium': {
+          'font-size': '1.0rem',
+          'font-weight': 500,
+          'line-height': '1.8rem',
+        },
+        '.body5-regular': {
+          'font-size': '1.0rem',
+          'font-weight': 400,
+          'line-height': '1.8rem',
+        },
+      });
+    },
+  ],
 };
