b148121c1f8da526ef2c332a0ea498fd6f917357
sobokki <jso4093@naver.com>
2024-03-17 17:55:55 +0900

    필요없는 코드 삭제

--- src/components/SchedulesPage/Schedules.tsx
+++ src/components/SchedulesPage/Schedules.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import clsx from 'clsx';
 import MyCalendar from '../../../public/assets/My calendar.svg';
 import CalendarImg from '../../../public/assets/calendar-dark.svg';
@@ -7,8 +7,6 @@ import Button from '../common/Button';
 import ControlDate from './ControlDate';
 import DateBox from './DateBox';
 import GruoupFilter from './GroupFilter';
-import axios from 'axios';
-import { calendarContext } from '@/contexts/CalenarProvider';
 
 // const requestData = {
 //   url: `https://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo`,
@@ -20,27 +18,8 @@ interface SchedulesProps {
 }
 
 function Schedules({ calendarType }: SchedulesProps) {
-  const { nowDate } = useContext(calendarContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const modalRef = useRef<HTMLDivElement>(null);
-  // const [holiday, setHoliday] = useState('');
-
-  // const getHoliday = async () => {
-  //   const bodyData = {
-  //     ...requestData,
-  //     solYear: nowDate.getFullYear(),
-  //     solMonth: nowDate.getMonth() + 1,
-  //   };
-  //   const response = await axios.get(
-  //     `${bodyData.url}?ServiceKey=${bodyData.serviceKey}&solYear=${bodyData.solYear}&solMonth=${bodyData.solMonth}`,
-  //   );
-  //   console.log(response.data.response.body.items);
-  // };
-
-  // useEffect(() => {
-  //   getHoliday();
-  // }, [nowDate]);
-
   const container =
     'w-[161.2rem] bg-[#F7F7F7] mt-[8.6rem] ml-[28.4rem] mb-[2.4rem]  mr-[2.4rem] pb-[3rem] pt-12 pl-12 pr-[26.9rem] rounded-[2.4rem]';
 
