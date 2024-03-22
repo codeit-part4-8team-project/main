7ab1c1d64e664e7a3d3e8642172cb761e2094e27
sobokki <145126857+sobokki@users.noreply.github.com>
2024-03-22 09:13:27 +0900

    Feat: mock데이터로 월별 캘린더 데이터 넣기, 필터체크 아이콘 컴포넌트 추가, 캘린더 UI 수정 (#65)

--- src/components/SchedulesPage/GroupFilter.tsx
+++ src/components/SchedulesPage/GroupFilter.tsx
@@ -1,62 +1,69 @@
 import { useState } from 'react';
-import BaseProfile from '../../../public/assets/ProfileIcon.svg';
-import CheckOn from '../../../public/assets/check-circle-fill-dark.svg';
-import CheckOff from '../../../public/assets/check-circle-fill.svg';
+import FilterCheckIconDark from '@/assets/FilterCheckIconDark';
+import FilterCheckIconLight from '@/assets/FilterCheckIconLight';
 
 interface ChecklistItemProps {
   isChecked: boolean;
-  name: string;
-  color: string;
-  onCheck: () => void;
+  title: string;
+  color?: string;
+  onCheck: (title: string, isChecked: boolean) => void;
 }
 
-function ChecklistItem({ name, color, isChecked, onCheck }: ChecklistItemProps) {
+function ChecklistItem({ title, color, isChecked, onCheck }: ChecklistItemProps) {
   const nameStyle = 'text-body4-bold ml-4 text-[#292929]';
   return (
-    <div className="flex w-[17.3rem] items-center justify-between">
+    <div className="mr-[7.4rem] flex w-[17.3rem] items-center justify-between">
       <div className="flex">
         {color ? (
           <div className={` h-4 w-4 rounded-full`} style={{ backgroundColor: color }}></div>
         ) : (
-          <img className="h-4 w-4" src={BaseProfile} alt="기본 프로필" />
+          <div className={` h-4 w-4 rounded-full`} style={{ backgroundColor: 'black' }}></div>
         )}
-        <span className={nameStyle}>{name}</span>
+        <span className={nameStyle}>{title}</span>
       </div>
-      <img onClick={onCheck} src={isChecked ? CheckOn : CheckOff} alt="체크리스트" />
+      <div onClick={() => onCheck(title, !isChecked)}>
+        {isChecked ? <FilterCheckIconDark /> : <FilterCheckIconLight />}
+      </div>
     </div>
   );
 }
-function GroupFilter() {
-  const items = [
-    { name: '홍길동', color: '' },
-    { name: '디자인 스터디', color: 'yellow' },
-    { name: '코드잇 프로젝트', color: 'blue' },
-    { name: '토익 공부 모임', color: 'red' },
-  ];
 
-  const [checkedItems, setCheckedItems] = useState<boolean[]>(Array(items.length).fill(false));
+export interface Item {
+  title: string;
+  color?: string;
+}
 
-  const handleCheck = (index: number) => {
-    setCheckedItems((prev) => {
-      const newCheckedItems = [...prev];
-      newCheckedItems[index] = !newCheckedItems[index];
-      return newCheckedItems;
-    });
+interface GroupFilterProps {
+  items: Item[];
+  onCheck: (checkedItem: string[]) => void;
+}
+
+function GroupFilter({ items, onCheck }: GroupFilterProps) {
+  const [checkedItems, setCheckedItems] = useState<string[]>([]);
+
+  const handleCheck = (title: string, isChecked: boolean) => {
+    const updatedCheckedItems = isChecked
+      ? [...checkedItems, title]
+      : checkedItems.filter((item) => item !== title);
+    setCheckedItems(updatedCheckedItems);
+    onCheck(updatedCheckedItems);
   };
 
   return (
-    <div className="flex flex-col">
-      <div className="text-body3-bold mb-14 text-[#A1A1A1]">그룹 필터</div>
-      {items.map((item, index) => (
-        <ChecklistItem
-          key={index}
-          name={item.name}
-          color={item.color}
-          isChecked={checkedItems[index]}
-          onCheck={() => handleCheck(index)}
-        />
-      ))}
-    </div>
+    <>
+      <div className="flex flex-col">
+        <div className="mb-14 text-body3-bold text-[#A1A1A1]">그룹 필터</div>
+        {items.map((data, index) => (
+          <ChecklistItem
+            key={index}
+            title={data.title}
+            color={data.color}
+            isChecked={checkedItems.includes(data.title)}
+            onCheck={handleCheck}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/components/SchedulesPage/Schedules.tsx
+++ src/components/SchedulesPage/Schedules.tsx
@@ -1,19 +1,32 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import clsx from 'clsx';
 import ScheduleModal from '../Modal/ScheduleModal';
 import Button from '../common/Button';
 import ControlDate from './ControlDate';
 import DateBox from './DateBox';
-import GruoupFilter from './GroupFilter';
+import { Item } from './GroupFilter';
+import GroupFilter from './GroupFilter';
+import axios from 'axios';
 import CalendarIcon from '@/assets/CalendarIcon';
 
 interface SchedulesProps {
   calendarType: string;
+  schedules?: Schedules[];
+}
+export interface Schedules {
+  title: string;
+  content: string;
+  startDateTime: Date;
+  endDateTime: Date;
 }
 
 function Schedules({ calendarType }: SchedulesProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const modalRef = useRef<HTMLDivElement>(null);
+  const [groupData, setGroupData] = useState<Item[]>([]);
+  const [schedules, setSchedules] = useState<Schedules[]>([]);
+  const [filteredSchedules, setFilteredSchedules] = useState<Schedules[]>([]);
+
   const container =
     'w-[161.2rem] bg-[#F7F7F7] mt-[8.6rem] ml-[28.4rem] mb-[2.4rem]  mr-[2.4rem] pb-[3rem] pt-12 pl-12 pr-[26.9rem] rounded-[2.4rem]';
 
@@ -30,14 +43,68 @@ function Schedules({ calendarType }: SchedulesProps) {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
   let calendarsType = calendarType === '나의 캘린더' ? 'My Calendar' : 'Team Calendar ';
+
+  useEffect(() => {
+    // calendarType에 따라 다른 엔드포인트를 호출하여 데이터를 가져옴
+    const fetchData = async () => {
+      try {
+        let endpoint = '';
+        if (calendarType === '나의 캘린더') {
+          endpoint = '/public/data/GroupFilter.json';
+        } else {
+          endpoint = '/public/data/GroupFilterTeam.json';
+        }
+
+        // 상태(state)가 비어 있는 경우에만 데이터를 불러옴
+        if (groupData.length === 0) {
+          const response = await axios.get(endpoint);
+          setGroupData(response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching group filter data:', error);
+      }
+    };
+
+    fetchData();
+  }, [calendarType, groupData]); // groupData 상태 추가
+
+  useEffect(() => {
+    // calendarType에 따라 다른 엔드포인트를 호출하여 데이터를 가져옴
+    const fetchSchedule = async () => {
+      try {
+        let endpoint = '';
+        if (calendarType === '나의 캘린더') {
+          endpoint = '/public/data/IdSchedule.json';
+        } else {
+          endpoint = '/public/data/TeamSchedule.json';
+        }
+
+        // 상태(state)가 비어 있는 경우에만 데이터를 불러옴
+        if (schedules.length === 0) {
+          const response = await axios.get(endpoint);
+          setSchedules(response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching group filter data:', error);
+      }
+    };
+
+    fetchSchedule();
+  }, [calendarType, schedules]); // schedules 상태 추가
+
+  const handleCheck = (checkedItems: string[]) => {
+    // 그룹 필터 아이템들의 타이틀들을 전달하여 필터링된 일정을 업데이트
+    setFilteredSchedules(schedules.filter((schedule) => checkedItems.includes(schedule.title)));
+  };
 
   return (
     <div className={container}>
       <div className="m-0 flex items-center justify-between p-0 ">
         <div className={title}>
           <CalendarIcon active={true} className="mr-4 h-[3.6rem] w-[3.6rem]"></CalendarIcon>
-          <span className="mr-[2.4rem] whitespace-nowrap font-rammetto text-body1-medium">
+          <span className="mr-[10rem] whitespace-nowrap font-rammetto text-body1-medium">
             {calendarsType}
           </span>
           <ControlDate mode="month" />
@@ -47,8 +114,8 @@ function Schedules({ calendarType }: SchedulesProps) {
         {isModalOpen && <ScheduleModal />}
       </div>
       <div className="mr-[0.1rem] mt-[3.9rem] flex gap-12">
-        <GruoupFilter />
-        <DateBox mode="month" />
+        <GroupFilter items={groupData} onCheck={handleCheck} />
+        <DateBox schedules={filteredSchedules} mode="month" />
       </div>
     </div>
   );
