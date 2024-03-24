222fe0455f80be59d15967adf1148b4415398ef1
Jisun Park <jisunpark.t@gmail.com>
2024-03-25 00:00:28 +0900

    Fix: Remove TabProvider from TeamLayout

--- src/components/TeamsPage/TeamLayout.tsx
+++ src/components/TeamsPage/TeamLayout.tsx
@@ -3,19 +3,16 @@ import Board from '@/components/common/Board';
 import Nav from '@/components/common/Nav';
 import SideBar from '@/components/common/sideBar/SideBar';
 import TeamBar from '@/components/TeamsPage/TeamBar';
-import { TabProvider } from '@/contexts/TabProvider';
 
 export default function TeamLayout() {
   return (
-    <TabProvider>
-      <div className="h-screen w-screen bg-gray20 pb-[4.4rem] pl-[28.4rem] pr-[2.4rem] pt-[14.2rem]">
-        <Nav />
-        <SideBar />
-        <TeamBar />
-        <Board>
-          <Outlet />
-        </Board>
-      </div>
-    </TabProvider>
+    <div className="h-screen w-screen bg-gray20 pb-[4.4rem] pl-[28.4rem] pr-[2.4rem] pt-[14.2rem]">
+      <Nav />
+      <SideBar />
+      <TeamBar />
+      <Board>
+        <Outlet />
+      </Board>
+    </div>
   );
 }
