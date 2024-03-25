75d34365b0cdf157fc79273cb1c987a31920762e
Jisun Park <jisunpark.t@gmail.com>
2024-03-25 10:54:29 +0900

    Design: Apply common layout

--- src/pages/MyPostsPage.tsx
+++ src/pages/MyPostsPage.tsx
@@ -4,11 +4,9 @@ import BulletinBoard from '@/components/posts/BulletinBoard';
 
 export default function MyPostsPage() {
   return (
-    <div>
-      <BoardSection
-        title="Bulletin board"
-        content={<BulletinBoard posts={teamPosts.posts.content} />}
-      />
-    </div>
+    <BoardSection
+      title="Bulletin board"
+      content={<BulletinBoard posts={teamPosts.posts.content} />}
+    />
   );
 }
