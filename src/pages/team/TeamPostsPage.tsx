770be8b0c077007be0e4af267c7175362fe7ab3e
Jisun Park <jisunpark.t@gmail.com>
2024-03-26 22:22:43 +0900

    Fix: Remove unnecessary parameters on usePost

--- src/pages/team/TeamPostsPage.tsx
+++ src/pages/team/TeamPostsPage.tsx
@@ -5,7 +5,7 @@ import usePost from '@/hooks/usePost';
 
 export default function TeamPostsPage() {
   const { currentTeam } = useTeam();
-  const { postData: teamPosts } = usePost({ type: 'team', teamId: currentTeam.id });
+  const { postData: teamPosts } = usePost({ teamId: currentTeam.id });
 
   return <BoardSection title="Bulletin board" content={<PostList posts={teamPosts.content} />} />;
 }
