b81aa23c39e6658643e2516c52757c21039618a7
Jisun Park <jisunpark.t@gmail.com>
2024-03-29 19:08:04 +0900

    Refactor: Unify page constants to PAGES and redefine Pages type

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,37 +1,102 @@
 import { ReactNode } from 'react';
-import MainPage from '@/pages/MainPage';
-import MyIssuesPage from '@/pages/MyIssuesPage';
-import MyPage from '@/pages/MyPage';
-import MyPostsPage from '@/pages/MyPostsPage';
 import SchedulesPage from '@/pages/SchedulesPage';
-import TeamAnnouncementPage from '@/pages/team/TeamAnnouncementPage';
+import UserIssuesPage from '@/pages/UserIssuesPage';
+import UserMainPage from '@/pages/UserMainPage';
+import UserMyPage from '@/pages/UserMyPage';
+import UserPostsPage from '@/pages/UserPostsPage';
+import TeamAnnouncementsPage from '@/pages/team/TeamAnnouncementsPage';
 import TeamCalendar from '@/pages/team/TeamCalendar';
-import TeamHome from '@/pages/team/TeamHome';
 import TeamIssuesPage from '@/pages/team/TeamIssuesPage';
+import TeamMainPage from '@/pages/team/TeamMainPage';
 import TeamMembers from '@/pages/team/TeamMembers';
 import TeamPostsPage from '@/pages/team/TeamPostsPage';
+import BoxIcon from '@/assets/BoxIcon';
+import CalendarIcon from '@/assets/CalendarIcon';
+import FolderIcon from '@/assets/FolderIcon';
+import MegaphoneIcon from '@/assets/MegaphoneIcon';
+import PeopleIcon from '@/assets/PeopleIcon';
+import ViewListIcon from '@/assets/ViewListIcon';
+
+export type UserPageType = 'main' | 'schedules' | 'issues' | 'posts' | 'myPage';
+export type TeamPageType = 'main' | 'schedules' | 'issues' | 'posts' | 'announcements' | 'members';
+// type PageType = UserPageType | TeamPageType; /* 필요할 때 주석 해제 */
+
+export interface Page {
+  title: string;
+  page: ReactNode;
+  icon: ReactNode;
+}
+export type UserPage = {
+  [page in UserPageType]: Page;
+};
+
+export type TeamPage = {
+  [page in TeamPageType]: Page;
+};
 
 export interface Pages {
-  [page: string]: ReactNode;
+  user: UserPage;
+  team: TeamPage;
 }
 
-export const PAGES: {
-  user: Pages;
-  team: Pages;
-} = {
+export const PAGES: Pages = {
   user: {
-    main: <MainPage />,
-    schedules: <SchedulesPage />,
-    issues: <MyIssuesPage />,
-    posts: <MyPostsPage />,
-    myPage: <MyPage />,
+    main: {
+      title: '대시보드',
+      page: <UserMainPage />,
+      icon: <BoxIcon />,
+    },
+    schedules: {
+      title: '나의 캘린더',
+      page: <SchedulesPage />,
+      icon: <CalendarIcon />,
+    },
+    issues: {
+      title: '칸반보드',
+      page: <UserIssuesPage />,
+      icon: <ViewListIcon />,
+    },
+    posts: {
+      title: '자유게시판',
+      page: <UserPostsPage />,
+      icon: <FolderIcon />,
+    },
+    myPage: {
+      title: '마이페이지',
+      page: <UserMyPage />,
+      icon: <CalendarIcon /> /* TODO 디자인 나오는대로 아이콘 변경 필요 */,
+    },
   },
   team: {
-    main: <TeamHome />,
-    schedules: <TeamCalendar />,
-    issues: <TeamIssuesPage />,
-    announcements: <TeamAnnouncementPage />,
-    posts: <TeamPostsPage />,
-    // members: <TeamMembers />,
+    main: {
+      title: '홈',
+      page: <TeamMainPage />,
+      icon: <BoxIcon />,
+    },
+    schedules: {
+      title: '팀 캘린더',
+      page: <TeamCalendar />,
+      icon: <CalendarIcon />,
+    },
+    issues: {
+      title: '칸반보드',
+      page: <TeamIssuesPage />,
+      icon: <ViewListIcon />,
+    },
+    announcements: {
+      title: '공지사항',
+      page: <TeamAnnouncementsPage />,
+      icon: <MegaphoneIcon />,
+    },
+    posts: {
+      title: '자유게시판',
+      page: <TeamPostsPage />,
+      icon: <FolderIcon />,
+    },
+    members: {
+      title: '팀원',
+      page: <TeamMembers />,
+      icon: <PeopleIcon />,
+    },
   },
 };
