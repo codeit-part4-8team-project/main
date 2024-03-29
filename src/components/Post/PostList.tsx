7a1a1db10e58a8bc0ec0f185f9022ad58b440b7b
Jisun Park <jisunpark.t@gmail.com>
2024-03-29 23:05:28 +0900

    Fix: Add key props in PostList

--- src/components/Post/PostList.tsx
+++ src/components/Post/PostList.tsx
@@ -8,18 +8,18 @@ interface PostListProps {
 
 export default function PostList({ posts }: PostListProps) {
   return (
-    <li className="mt-[1.4rem] grid w-fit grid-cols-2 gap-12">
+    <ul className="mt-[1.4rem] grid w-fit grid-cols-2 gap-12">
       {posts.length !== 0 ? (
         posts.map((post) => {
           return (
-            <ul key={post.id}>
+            <li key={post.id}>
               <PostItem post={post} />
-            </ul>
+            </li>
           );
         })
       ) : (
         <NoCard backgroundColor="bg-white">게시글이 없습니다.</NoCard>
       )}
-    </li>
+    </ul>
   );
 }
