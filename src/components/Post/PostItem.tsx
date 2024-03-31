a0b32c9aeff70b95d02d48fe29c69496c42b8fbd
Jisun Park <jisunpark.t@gmail.com>
2024-03-31 22:08:48 +0900

    Design: Apply changed ui of PostItem

--- src/components/Post/PostItem.tsx
+++ src/components/Post/PostItem.tsx
@@ -9,27 +9,40 @@ interface PostItemProps {
 }
 
 export default function PostItem({ post }: PostItemProps) {
-  const { id, author, content, createdDate, liked, likeCount } = post;
+  const { id, author, title, content, createdDate, liked, likeCount, team } = post;
+
+  const { color, name } = team;
 
   return (
-    <div className="relative flex h-[23.3rem] max-w-[58rem] flex-col gap-[2.4rem] rounded-[2.4rem] border border-gray30 bg-white p-[2.4rem]">
+    <div className="relative flex h-[23.3rem] w-[58rem] flex-col gap-[2.4rem] rounded-[2.4rem] border border-gray30 bg-white p-[2.4rem]">
       <button type="button" className="absolute right-[2.4rem] top-[2.4rem]">
         <MeatbollsIcon />
       </button>
-      <div className="flex items-center gap-[1.5rem]">
+      <div className="flex h-[3.6rem] items-center gap-[1.5rem]">
         <img
           src={author.imageUrl || ProfileImg}
           alt="유저 프로필 이미지"
           className="h-[3.6rem] w-[3.6rem] rounded-full"
         />
-        <div className="flex flex-col gap-[0.4rem]">
-          <span className="text-body4-regular text-gray100">#{author.name}</span>
+        <div className="flex flex-col items-start gap-[0.4rem]">
+          <div className="flex gap-[1.6rem]">
+            <span className="text-body4-regular text-gray100">#{author.name}</span>
+            <button
+              style={{ color }} /* TODO 임시 */
+              className={`bottom-8 left-8 flex h-[1.8rem] items-center justify-center rounded-[4rem] border px-4 py-[0.6rem] text-[0.8rem] font-medium`}
+            >
+              {name}
+            </button>
+          </div>
           <span className="text-body4-regular text-gray50">{toDateFormat(createdDate)}</span>
         </div>
       </div>
-      <span className="inline-block h-[7.7rem] w-full max-w-[44.9rem] overflow-auto text-body4-regular text-gray100">
-        {content}
-      </span>
+      <div className="flex flex-col gap-[0.8rem]">
+        <span className="text-body4-bold text-gray100">{title}</span>
+        <span className="inline-block h-[5.9rem] w-full overflow-auto text-body4-regular text-gray100">
+          {content}
+        </span>
+      </div>
       <div className="absolute bottom-[2.4rem] left-[2.7rem]">
         <PostLike postId={id} liked={liked} likeCount={likeCount} />
       </div>
--- src/types/postTypes.ts
+++ src/types/postTypes.ts
@@ -1,12 +1,14 @@
 import { Author, PageableData } from '@/types/commonTypes';
+import { Team } from '@/types/teamTypes';
 
 export interface Post {
-  id: number;
+  id: 0;
   title: string;
   author: Author;
   content: string;
   createdDate: string;
-  likeCount: number;
+  likeCount: 0;
+  team: Team;
   edited: boolean;
   liked: boolean;
 }
