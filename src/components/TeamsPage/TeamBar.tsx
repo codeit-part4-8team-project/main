d4c53c5120de95ea94a2156b4c0b66bad18a0478
Jisun Park <jisunpark.t@gmail.com>
2024-03-31 14:06:34 +0900

    Feat: Add ProfileStack on TeamBar

--- src/components/TeamsPage/TeamBar.tsx
+++ src/components/TeamsPage/TeamBar.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import ProfileStack from '@/components/common/ProfileStack';
 import TextButton from '@/components/common/TextButton';
 import InvitationGroupModal from '@/components/Modal/InvitationGroupModal';
 import TeamTab from '@/components/TeamsPage/TeamTab';
 import { useTeam } from '@/contexts/TeamProvider';
+import { getProfilesImgs } from '@/lib/getProfileImgs';
 import ColorChipIcon from '@/assets/ColorChipIcon';
 import InviteIcon from '@/assets/InviteIcon';
 
@@ -19,8 +21,10 @@ export default function TeamBar() {
   if (!teamId) throw Error('해당 팀 ID가 존재하지 않습니다.');
 
   const {
-    team: { color, name },
+    team: { color, name, members },
   } = useTeam(teamId);
+
+  const profileImgs = members ? getProfilesImgs(members) : [];
 
   return (
     <div className="absolute left-[28.4rem] right-[2.4rem] top-[8.5rem] grid grid-cols-[14rem_auto_auto] items-center gap-[6.2rem]">
@@ -30,15 +34,18 @@ export default function TeamBar() {
         <span className="text-body1-bold text-gray100">{name}</span>
       </div>
       <TeamTab />
-      <TextButton
-        buttonSize="sm"
-        color="white"
-        onClick={handleToggleModalClick}
-        className="flex h-[3.6rem] w-[8.7rem] gap-[0.8rem] justify-self-end border-0"
-      >
-        <InviteIcon />
-        <span>초대</span>
-      </TextButton>
+      <div className="flex items-center gap-[1.6rem] justify-self-end">
+        <TextButton
+          buttonSize="sm"
+          color="white"
+          onClick={handleToggleModalClick}
+          className="flex h-[3.6rem] w-[8.7rem] gap-[0.8rem] border-0"
+        >
+          <InviteIcon />
+          <span>초대</span>
+        </TextButton>
+        <ProfileStack profileImgs={profileImgs} size="lg" />
+      </div>
     </div>
   );
 }
