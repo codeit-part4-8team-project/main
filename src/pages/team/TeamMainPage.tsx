0293e613a02c032ed379575b4059fb6be91b6a50
Jisun Park <jisunpark.t@gmail.com>
2024-04-01 22:46:39 +0900

    Fix: Fix data update issue at main pages

--- src/pages/team/TeamMainPage.tsx
+++ src/pages/team/TeamMainPage.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import BoardSection from '@/components/common/BoardSection';
 import FloatingButton from '@/components/common/FloatingButton';
@@ -14,8 +15,13 @@ export default function TeamMainPage() {
   if (!teamId) throw Error('해당 팀 ID가 존재하지 않습니다.');
 
   const { team } = useTeam(teamId);
-  const { issueBoardData } = useIssueBoard();
-  const { announcementData } = useAnnouncement();
+  const { issueBoardData, fetchIssueBoardData } = useIssueBoard();
+  const { announcementData, fetchAnnouncementData } = useAnnouncement();
+
+  useEffect(() => {
+    fetchAnnouncementData();
+    fetchIssueBoardData();
+  }, [teamId]);
 
   return (
     <>
