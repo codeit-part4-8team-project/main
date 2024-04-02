e1f708ba318c88376a7220f997302e1d0fde6aa7
sobokki <jso4093@naver.com>
2024-04-02 17:33:37 +0900

    Time 컴포넌트 수정

--- src/components/SchedulesPage/Tiem.tsx
+++ src/components/SchedulesPage/Tiem.tsx
@@ -1,57 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TextButton from '../common/TextButton';
 import AllowDownIcon from '@/assets/AllowDownIcon';
 
 interface NavItemProps {
   icon: React.ReactNode;
   children: React.ReactNode[];
-  onItemSelected: (item: string) => void;
 }
 
-function NavItem({ icon, children, onItemSelected }: NavItemProps) {
+function NavItem({ icon, children }: NavItemProps) {
   const [open, setOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState('');
 
   const toggleOpen = () => {
     setOpen(!open);
   };
 
-  const firstChild = children[0];
-
-  const handleItemClick = (item: string | null | undefined) => {
-    console.log('Item clicked:', item);
-    if (item) {
-      onItemSelected(item);
-      setOpen(false); // Close dropdown after selection if you want
-    }
+  const handleItemClick = (item: string) => {
+    setSelectedItem(item);
+    setOpen(false); // Close dropdown after selection
   };
 
   return (
     <div className="nav-item-container relative">
       <div className="flex h-[3.2rem] w-24 items-center rounded-[0.6rem] border-[0.1rem] border-gray30 py-[1.2rem] pl-4">
-        {!open && <div className="nav-item-content">{firstChild}</div>}
+        <div className="nav-item-content " onClick={toggleOpen}>
+          {selectedItem}
+        </div>
         {open && (
-          <div className="absolute left-0 right-0 top-[3.2rem] z-50 bg-white">
-            {open && (
-              <div className="z-50 bg-white">
-                {children.map((item, index) => (
-                  <div
-                    key={index}
-                    onClick={() =>
-                      handleItemClick(
-                        typeof item === 'object' ? ((item as any)?.props?.children as string) : '',
-                      )
-                    }
-                    className={`cursor-pointer ${
-                      (typeof item === 'object' ? (item as any)?.props?.children : '')
-                        ? 'bg-gray300'
-                        : ''
-                    }`}
-                  >
-                    {item}
-                  </div>
-                ))}
+          <div className="  absolute left-0 right-0 top-[3.2rem] z-50 bg-white">
+            {children.map((item, index) => (
+              <div
+                key={index}
+                onClick={() => handleItemClick(item as string)}
+                className={`max-h-[5rem] cursor-pointer overflow-y-auto ${
+                  selectedItem ===
+                  (typeof item === 'object' ? ((item as any)?.props?.children as string) : '')
+                    ? 'bg-gray300'
+                    : ''
+                }`}
+              >
+                {item}
               </div>
-            )}
+            ))}
           </div>
         )}
         <button className="icon-button" onClick={toggleOpen}>
@@ -65,11 +55,6 @@ function NavItem({ icon, children, onItemSelected }: NavItemProps) {
 export default NavItem;
 
 export function Time() {
-  const [selectedItem, setSelectedItem] = useState('');
-  const handleItemSelected = (item: string) => {
-    setSelectedItem(item);
-  };
-
   const times = Array.from({ length: 12 }, (_, index) => index + 1);
   const minutes = Array.from({ length: 60 }, (_, index) => index.toString().padStart(2, '0'));
 
@@ -77,11 +62,11 @@ export function Time() {
     <div className="flex flex-col items-center">
       <p className="mb-[0.2rem] mt-6 text-body5-bold text-gray100">시간</p>
       <div className="flex items-center  gap-[0.9rem]">
-        <NavItem onItemSelected={handleItemSelected} icon={<AllowDownIcon />}>
+        <NavItem icon={<AllowDownIcon />}>
           <p className="text-body5-bold text-gray100">AM</p>
           <p className="text-body5-bold text-gray100">PM</p>
         </NavItem>
-        <NavItem onItemSelected={handleItemSelected} icon={<AllowDownIcon />}>
+        <NavItem icon={<AllowDownIcon />}>
           {times.map((number) => (
             <p className="text-body5-bold text-gray100" key={number}>
               {number}
@@ -89,15 +74,12 @@ export function Time() {
           ))}
         </NavItem>
         <div className="text-body5-bold text-gray100">:</div>
-        <NavItem onItemSelected={handleItemSelected} icon={<AllowDownIcon />}>
-          <div></div>
-          <div className="max-h-[5rem] overflow-y-auto">
-            {minutes.map((number) => (
-              <p className="text-body5-bold text-gray100" key={number}>
-                {number}
-              </p>
-            ))}
-          </div>
+        <NavItem icon={<AllowDownIcon />}>
+          {minutes.map((number) => (
+            <p className="text-body5-bold text-gray100" key={number}>
+              {number}
+            </p>
+          ))}
         </NavItem>
       </div>
       <TextButton
