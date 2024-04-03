59da04a9dc58a8b0a6b8a267000c1e509fd7ff46
caseV12 <caseVDev@gmail.com>
2024-04-03 17:55:49 +0900

    Fix: resolve complie error before build

--- src/components/common/TextButton.tsx
+++ src/components/common/TextButton.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, MouseEventHandler, ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import clsx from 'clsx';
 
 //사이즈 컬러 별로 분기, classname 받아서 부모에서 스타일 추가 가능하도록
@@ -8,7 +8,6 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   buttonSize: 'sm' | 'md' | 'lg';
   color?: 'black' | 'white' | 'red';
   children: ReactNode;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
   disabled?: boolean;
   className?: string;
 }
