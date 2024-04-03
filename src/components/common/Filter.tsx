c50ed9449016509bcd5d17723203122a3dcf76ef
Jisun Park <jisunpark.t@gmail.com>
2024-04-03 17:24:47 +0900

    Feat: Add states to manage checked items

--- src/components/common/Filter.tsx
+++ src/components/common/Filter.tsx
@@ -6,13 +6,15 @@ import ColorChipIcon from '@/assets/ColorChipIcon';
 
 interface FilterItemProps {
   team: Team;
-  isChecked: boolean;
+  checkedTeamId: number[];
+  setCheckedTeamId: (checked: number[]) => void;
 }
 
 export default function Filter() {
-  const [teams, setTeams] = useState<Team[] | []>([]);
+  const [teams, setTeams] = useState<Team[]>([]);
+  const [checkedTeamId, setCheckedTeamId] = useState<number[]>([]);
 
-  const { loading, error, data } = useAxios<Team[] | []>(
+  const { loading, error, data } = useAxios<Team[]>(
     {
       path: '/team/',
       method: 'GET',
@@ -32,19 +34,45 @@ export default function Filter() {
       <span className="text-body3-bold text-gray50">그룹 필터</span>
       <ul className="flex flex-col gap-[1.3rem]">
         {teams.map((team) => {
-          return <FilterItem team={team} isChecked={true}></FilterItem>;
+          return (
+            <FilterItem
+              key={team.id}
+              team={team}
+              checkedTeamId={checkedTeamId}
+              setCheckedTeamId={setCheckedTeamId}
+            ></FilterItem>
+          );
         })}
       </ul>
     </div>
   );
 }
 
-function FilterItem({ team: { id, name, color }, isChecked }: FilterItemProps) {
+function FilterItem({
+  team: { id, name, color },
+  // isChecked,
+  checkedTeamId,
+  setCheckedTeamId,
+}: FilterItemProps) {
+  const [isChecked, setIsChecked] = useState(checkedTeamId.includes(id));
+
+  const handleCheckClick = () => {
+    if (!isChecked) {
+      setCheckedTeamId([...checkedTeamId, id]);
+    } else {
+      const deleted = checkedTeamId.filter((checkedId) => checkedId !== id);
+      setCheckedTeamId([...deleted]);
+    }
+    setIsChecked(!isChecked);
+  };
+
   return (
     <li key={id} className="flex w-[19rem] items-center justify-between gap-4">
       <ColorChipIcon fill={color} />
       <span className="grow justify-self-start text-body4-bold">{name}</span>
-      <CheckCircleIcon active={isChecked} />
+      <button type="button" onClick={handleCheckClick}>
+        <CheckCircleIcon active={isChecked} />
+      </button>
     </li>
   );
 }
