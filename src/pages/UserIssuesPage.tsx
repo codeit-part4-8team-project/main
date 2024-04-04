661497c7b5358220034f2b843f10c2db9c0a8595
Jisun Park <jisunpark.t@gmail.com>
2024-04-04 17:48:43 +0900

    Feat: Apply groupfilter in UserIssuesPage

--- src/pages/UserIssuesPage.tsx
+++ src/pages/UserIssuesPage.tsx
@@ -1,15 +1,27 @@
+import { useEffect, useState } from 'react';
 import BoardSection from '@/components/common/BoardSection';
+import Filter from '@/components/common/Filter';
 import KanbanBoard from '@/components/kanbanBoard/KanbanBoard';
 import { IssueProvider } from '@/contexts/IssueProvider';
 import { useIssueBoard } from '@/hooks/useIssue';
+import { Issues } from '@/types/issueTypes';
 
 export default function UserIssuesPage() {
-  const { issueBoardData } = useIssueBoard();
+  const { issueBoardData, checkedTeamId, setCheckedTeamId } = useIssueBoard();
+  const [issueData, setIssueData] = useState<Issues>(issueBoardData);
+
+  useEffect(() => {
+    setIssueData(issueBoardData);
+  }, [checkedTeamId, issueBoardData]);
 
   return (
     <BoardSection title="Kanban board">
       <IssueProvider>
-        <KanbanBoard issues={issueBoardData} type="page" />
+        <Filter
+          checkedTeamId={checkedTeamId || []}
+          setCheckedTeamId={setCheckedTeamId || (() => {})}
+        />
+        <KanbanBoard issues={issueData} type="page" />
       </IssueProvider>
     </BoardSection>
   );
