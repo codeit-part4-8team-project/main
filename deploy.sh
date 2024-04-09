c9e8c2c4cc27bdfc0afbfffd575d5967454ce471
Jisun Park <jisunpark.t@gmail.com>
2024-04-09 17:36:56 +0900

    Test5

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu/main
 git pull origin main
-sudo npm install
+npm install
 rm -rf dist 
-sudo npm run build
+npm run build
 sudo systemctl stop nginx
 sudo systemctl start nginx
