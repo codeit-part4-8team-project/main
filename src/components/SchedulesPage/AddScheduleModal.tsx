bb6187fe660ee698d077c9a6b4b141c83ff00303
sobokki <145126857+sobokki@users.noreply.github.com>
2024-04-12 11:31:26 +0900

    코드정리 (#160)
    * 코드정리
    
    * Fixed: AllDay 스캐줄 더 보기 모달 수정 및 완료
    
    * 코드 수정

--- src/components/SchedulesPage/AddScheduleModal.tsx
+++ src/components/SchedulesPage/AddScheduleModal.tsx
@@ -1,30 +1,53 @@
+import { MouseEvent } from 'react';
 import { Schedule } from '@/contexts/CalenarProvider';
 
 interface AddScheduleModalProps {
-  className: string;
   onClick: () => void;
   content: Schedule[];
+  onClose: () => void;
 }
 
-export default function AddScheduleModal({ className, onClick, content }: AddScheduleModalProps) {
+export default function AddScheduleModal({ content, onClick, onClose }: AddScheduleModalProps) {
+  const handleCloseModal = (e: MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      // 모달 닫기 함수 호출
+      onClick();
+    }
+    // 모달 닫기 함수 호출
+  };
+  const handleModalClick = (e: MouseEvent<HTMLDivElement>) => {
+    // 모달 요소를 클릭했을 때는 클릭 이벤트를 중단하여 모달이 닫히지 않도록 합니다.
+    e.stopPropagation();
+  };
   return (
-    <div onClick={onClick} className={`z-60 ${className}`}>
-      <div className=" bg-white px-[1rem]  py-[0.4rem] shadow-xl">
-        <div className="relative">
-          <div className="flex flex-col gap-[1.5rem]">
-            {content.map((schedule, index) => (
-              <div key={index} className="schedule-item">
+    <div
+      className={`absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center  bg-[#FCFCFC]`}
+      onClick={handleCloseModal}
+    >
+      <div
+        className=" relative  z-[60]  h-[15.4rem] w-60 overflow-y-auto  rounded-lg py-[0.4rem] shadow-xl"
+        onClick={handleModalClick}
+      >
+        <div className="flex flex-col gap-4">
+          <span className="close absolute right-2 top-2 cursor-pointer" onClick={onClose}>
+            &times;
+          </span>
+          {content.map((schedule, index) => (
+            <div key={index} className="flex flex-col items-center  gap-2">
+              <div className="flex gap-2">
                 <div
                   className="h-5 w-5 rounded-full"
                   style={{ backgroundColor: schedule.team?.color || 'black' }}
                 ></div>
-                <div className="schedule-details">
-                  <p className="text-gray100">{schedule.user?.name || schedule.team?.name}</p>
-                  <p className="text-gray50">{schedule.title}</p>
-                </div>
+
+                <p className="text-gray-800  text-body4-bold">
+                  {schedule.user?.name || schedule.team?.name}
+                </p>
               </div>
-            ))}
-          </div>
+
+              <div className=" text-body3-regular text-gray50">{schedule.title}</div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/components/common/NavModal.tsx
@@ -0,0 +1,35 @@
+import { ReactNode, useState } from 'react';
+
+interface ModalProps {
+  onClose: () => void;
+  children: ReactNode;
+  onClick: () => void;
+}
+
+const NavModal: React.FC<ModalProps> = ({ onClose, children, onClick }) => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(true);
+  };
+  const handleConfirm = () => {
+    return clicked ? 'w-[0.5rem] h-[0.5rem] bg-point-red rounded-full' : '';
+  };
+
+  return (
+    <div className="modal absolute bottom-0 left-[-28.5rem] right-0 top-[9.7rem] z-50 flex flex-col justify-center">
+      <div
+        className="modal-content relative h-36 w-[38.4rem] rounded-lg bg-point_red"
+        onClick={onClick}
+      >
+        <div onClick={handleClick} className={handleConfirm()}></div>
+        {children}
+        <span className="close absolute right-2 top-2 cursor-pointer" onClick={onClose}>
+          &times;
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default NavModal;
