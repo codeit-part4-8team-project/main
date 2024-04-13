dc1df457d86688e5f1fef92cedfbba92de001d37
caseV12 <casevdev@gmail.com>
2024-04-13 23:09:31 +0900

    Style: Update responsive design in SigninPage & SignupPage

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -2,30 +2,31 @@ import CompletionStepContent from '@/components/SignupPage/CompletionStepContent
 import EnterUserStepContent from '@/components/SignupPage/EnterUserStepContent';
 import SignupLayout from '@/components/SignupPage/SignupLayout';
 import ToSStepContent from '@/components/SignupPage/ToSStepContent';
-import { useStepContext } from '@/contexts/SignupStepProvider';
-import KeepyUppyIcon from '@/assets/KeepyUppyIcon';
-import KeepyUppyLogo from '@/assets/KeepyUppyLogo';
+import { StepProvider, useStepContext } from '@/contexts/SignupStepProvider';
 
-const SignupPage = () => {
-  const { step } = useStepContext();
+function SignupPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center bg-gray100 pt-[18.9rem]">
-      <div className="fixed top-0 flex h-[5.8rem] w-full items-center justify-center gap-[0.8rem] bg-gray10">
-        <KeepyUppyIcon />
-        <KeepyUppyLogo size="sm" />
-      </div>
+    <StepProvider>
       <SignupLayout>
-        {step === 1 && <ToSStepContent />}
-        {step === 2 && <EnterUserStepContent />}
-        {step === 3 && <CompletionStepContent />}
+        <StepContent />
       </SignupLayout>
-      <footer className="fixed bottom-0 flex h-[5.8rem] w-full items-center justify-center bg-gray10">
-        <span className="text-body4-bold text-gray50">
-          All Rights Reserved ⓒ 2024 Project Team8-Codeit KEEPY UPPY.
-        </span>
-      </footer>
-    </div>
+    </StepProvider>
   );
-};
+}
+
+function StepContent() {
+  const { step } = useStepContext();
+
+  switch (step) {
+    case 1:
+      return <ToSStepContent />;
+    case 2:
+      return <EnterUserStepContent />;
+    case 3:
+      return <CompletionStepContent />;
+    default:
+      return null;
+  }
+}
 
 export default SignupPage;
