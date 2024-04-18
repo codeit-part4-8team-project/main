60e155dfc279c14eb331ab5ba52608cae77f3e39
Jisun Park <jisunpark.t@gmail.com>
2024-04-18 22:51:56 +0900

    Design: Implement user interface of ChatInput

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,9 +1,11 @@
 import ChatHeader from '@/components/chat/ChatHeader';
+import ChatInput from '@/components/chat/ChatInput';
 
 export default function Chat() {
   return (
     <div className="relative flex h-[102.2rem] w-[49.7rem] border-[0.2rem] bg-white">
       <ChatHeader />
+      <ChatInput />
     </div>
   );
 }
