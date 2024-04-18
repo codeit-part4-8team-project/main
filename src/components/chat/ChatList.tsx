b75973f9560144944a0a34ff28b64a7bd4f2393a
Jisun Park <jisunpark.t@gmail.com>
2024-04-18 22:02:57 +0900

    Design: Implement user interface of ChatList

---
+++ src/components/chat/ChatList.tsx
@@ -0,0 +1,29 @@
+import TextButton from '@/components/common/TextButton';
+import ChatListItem from '@/components/chat/ChatListItem';
+import ChatIcon from '@/assets/ChatIcon';
+import CloseIcon from '@/assets/CloseIcon';
+
+export default function ChatList() {
+  return (
+    <div className="flex h-[102.2rem] w-[49.7rem] flex-col gap-[3.8rem] border-[0.2rem] bg-white px-16 pb-[5.2rem] pt-16">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-[0.7rem]">
+          <ChatIcon />
+          <span className="text-body1-bold">채팅 목록</span>
+        </div>
+        <button type="button">
+          <CloseIcon />
+        </button>
+      </div>
+      <div className="flex flex-auto flex-col gap-[1.6rem]">
+        <ChatListItem isActive={true} />
+        <ChatListItem />
+        <ChatListItem />
+        <ChatListItem />
+      </div>
+      <TextButton buttonSize="md" color="white">
+        1:1 채팅 생성하기
+      </TextButton>
+    </div>
+  );
+}
