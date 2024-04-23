b7803dcf501280e7930533141c24501ad3be9518
Jisun Park <jisunpark.t@gmail.com>
2024-04-23 09:33:41 +0900

    Design: Fix chat height

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -4,7 +4,7 @@ import ChatInput from '@/components/chat/ChatInput';
 
 export default function Chat() {
   return (
-    <div className="fixed right-0 top-[5.8rem] z-50 flex h-full w-[49.7rem] bg-white">
+    <div className="fixed bottom-0 right-0 top-[5.8rem] z-50 flex w-[49.7rem] bg-white">
       <ChatHeader />
       <ChatBody />
       <ChatInput />
--- src/components/chat/ChatList.tsx
+++ src/components/chat/ChatList.tsx
@@ -8,7 +8,7 @@ export default function ChatList() {
   const { handleCloseClick } = useChat();
 
   return (
-    <div className="fixed right-0 top-[5.8rem] z-50 flex h-full w-[49.7rem] flex-col gap-[3.8rem] bg-white px-16 pb-[5.2rem] pt-16">
+    <div className="fixed bottom-0 right-0 top-[5.8rem] z-50 flex w-[49.7rem] flex-col gap-[3.8rem] bg-white px-16 pb-[5.2rem] pt-16">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-[0.7rem]">
           <ChatIcon />
