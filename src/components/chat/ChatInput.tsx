a6b0ecbebd0d4f3756fde897c9cd59c8c1ff9023
Jisun Park <jisunpark.t@gmail.com>
2024-04-25 16:11:31 +0900

    Deisgn: Add shodow on ChatInput div

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -2,7 +2,7 @@ import SendIcon from '@/assets/SendIcon';
 
 export default function ChatInput() {
   return (
-    <div className="absolute bottom-0 left-0 right-0 bg-white px-12 pb-[5.4rem]">
+    <div className="absolute bottom-0 left-0 right-0 h-[12.2rem] bg-white px-12 pb-[5.4rem] pt-[2.4rem] shadow-[0_-5px_10px_0_rgba(0,0,0,0.02)]">
       <div className="flex h-[4.4rem] items-center justify-between rounded-[0.4rem] bg-gray10 px-8">
         <input
           placeholder="메세지를 입력하세요"
